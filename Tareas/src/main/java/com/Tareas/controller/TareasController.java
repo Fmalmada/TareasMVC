9bcf1a39baa0787180845c025c072cf6a2a94002
fmalmada <fmalmada@fi.uba.ar>
2023-02-07 02:53:03 -0300

    Validacion y fecha en formato "dd/MM/YYYY"

--- Tareas/src/main/java/com/Tareas/controller/TareasController.java
+++ Tareas/src/main/java/com/Tareas/controller/TareasController.java
@@ -1,38 +1,54 @@
 package com.Tareas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.Tareas.data.TareasRepository;
 import com.Tareas.modelo.Tarea;
 
 @Controller
+
 public class TareasController {
 	
 	@Autowired
 	private TareasRepository tareasRepo;
+	
+	ModelAndView modelo;
 
 	public TareasController(TareasRepository unRepo) {
 		this.tareasRepo = unRepo;
 	}
 	
 	@GetMapping("/")
-	public ModelAndView tareas() {
-		ModelAndView modelo = new ModelAndView("listaForm");
+	public ModelAndView tareas(Tarea unaTarea) {
+		modelo = new ModelAndView("listaForm");
 		modelo.addObject("tareas", tareasRepo.findAll());
 		modelo.addObject("prioridades", Tarea.Prioridad.values());
+		modelo.addObject("tarea", unaTarea);
 		return modelo;
 	}
 	
 	@PostMapping("/")
-	public String agregarTarea(Tarea nuevaTarea) {
+	public ModelAndView agregarTarea(@Valid @ModelAttribute("tarea") Tarea nuevaTarea, BindingResult resultados, Errors errores) {
+		if (errores.hasErrors()) {
+			ModelAndView nuevoModelo = new ModelAndView("listaForm", resultados.getModel());
+			nuevoModelo.addObject("tareas", tareasRepo.findAll());
+			nuevoModelo.addObject("prioridades", Tarea.Prioridad.values());
+			return nuevoModelo;
+		}
 		tareasRepo.save(nuevaTarea);
-		return "redirect:/";
-		
+		return tareas(new Tarea());
 	}
-
+		
 }
+
+
--- Tareas/src/main/java/com/Tareas/modelo/Tarea.java
+++ Tareas/src/main/java/com/Tareas/modelo/Tarea.java
@@ -1,11 +1,14 @@
 package com.Tareas.modelo;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -17,11 +20,14 @@ public class Tarea {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@NotNull(message="Se necesita una prioridad")
 	private Prioridad prioridad;
 	
+	@NotNull
+	@NotBlank(message = "Una descripcion es requerida")
 	private String descripcion;
 	
-	private Date creadaEn = new Date();
+	private String creadaEn = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
 	
 	private boolean finalizada = false;
 	
