e6602ff9756f9f96f844c10570c677a5b1cb6460
fmalmada <fmalmada@fi.uba.ar>
2023-02-08 01:15:45 -0300

    Se pueden eliminar tareas

--- Tareas/src/main/java/com/Tareas/controller/TareasController.java
+++ Tareas/src/main/java/com/Tareas/controller/TareasController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -78,12 +79,25 @@ public class TareasController {
 		return "redirect:/";
 	}
 	
+	@GetMapping("/eliminar/{id}")
+	public String eliminarTarea(@PathVariable("id") Long unaTareaId, Model unModelo) {
+		Optional<Tarea> posibleTarea = tareasRepo.findById(unaTareaId);
+		if (!posibleTarea.isPresent() ) {
+			throw new IllegalArgumentException("Tarea: " + unaTareaId + " no encontrada");
+		}
+		
+		tareasRepo.deleteById(unaTareaId);
+		return "redirect:/";
+		
+	}
 	
 	public ModelAndView cargarDatos(ModelAndView unModelo) {
 		unModelo.addObject("tareas", tareasRepo.findAll());
 		unModelo.addObject("prioridades", Tarea.Prioridad.values());
 		return unModelo;
 	}
+	
+	
 }
 
 
